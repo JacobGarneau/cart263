9610c9f33b87c63cb791744084bb11174764d12b
Jacob Garneau <jacobg17@hotmail.fr>
2021-02-17 16:48:16 -0500

    Activity 5: Drew finger pin
    Drew the finger pin which tracks the base and the tip of the index finger

--- activities/05-bubble-popper/js/script.js
+++ activities/05-bubble-popper/js/script.js
@@ -21,10 +21,11 @@ function preload() {}
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
   video.hide();
 
-  handpose = ml5.handpose(video, { flipHorizontal: true }, () => {
+  handpose = ml5.handpose(video, { flipHorizontal: false }, function () {
     console.log("Model ready");
   });
 
@@ -37,4 +38,41 @@ function setup() {
 /**
 Description of draw()
 */
-function draw() {}
+function draw() {
+  background(0);
+
+  imageMode(CENTER);
+  image(video, width / 2, height / 2);
+
+  if (predictions.length > 0) {
+    for (let i = 0; i < predictions.length; i++) {
+      let tipIndexFinger = {
+        x: predictions[i].annotations.indexFinger[3][0],
+        y: predictions[i].annotations.indexFinger[3][1],
+        z: predictions[i].annotations.indexFinger[3][2],
+      };
+      let baseIndexFinger = {
+        x: predictions[i].annotations.indexFinger[0][0],
+        y: predictions[i].annotations.indexFinger[0][1],
+        z: predictions[i].annotations.indexFinger[0][2],
+      };
+
+      push();
+      stroke(255);
+      strokeWeight(4);
+      line(
+        baseIndexFinger.x,
+        baseIndexFinger.y,
+        tipIndexFinger.x,
+        tipIndexFinger.y
+      );
+      pop();
+
+      push();
+      noStroke();
+      fill(255, 0, 0);
+      ellipse(baseIndexFinger.x, baseIndexFinger.y, 10, 10);
+      pop();
+    }
+  }
+}
