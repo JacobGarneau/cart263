02c53e236c1315f0f5ded24b1d4ce60db0e8c0e5
Jacob Garneau <jacobg17@hotmail.fr>
2021-02-17 15:55:08 -0500

    Activity 5: Added hand recognition
    Added code in setup() to enable hand detection through the webcam

--- activities/05-bubble-popper/js/script.js
+++ activities/05-bubble-popper/js/script.js
@@ -8,25 +8,33 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+let video;
+let handpose;
+let predictions = [];
+
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
 function setup() {
+  video = createCapture(VIDEO);
+  video.hide();
 
+  handpose = ml5.handpose(video, { flipHorizontal: true }, () => {
+    console.log("Model ready");
+  });
+
+  handpose.on(`predict`, (results) => {
+    predictions = results;
+    console.log(results);
+  });
 }
-
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
