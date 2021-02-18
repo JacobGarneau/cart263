2c9f1b304d6e353f86a4ae1431d4136daa61da49
Jacob Garneau <jacobg17@hotmail.fr>
2021-02-18 15:02:53 -0500

    E4: Started Exercise 4
    Copied files
    Changed HTML title
    Changed program description

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -1,11 +1,10 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+Bubble Popper ++
+Jacob Garneau
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Pop them bubbles!
 */
 
 let video;
