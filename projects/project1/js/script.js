d0e45c7547fc3a6e6e53fe9d43b6341fbbf31535
Jacob Garneau <jacobg17@hotmail.fr>
2021-02-24 20:04:10 -0500

    P1: Start of The Truman Game
    Copied files
    Changed description
    Changed HTML title

---
+++ projects/project1/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+Project 1 - A Night At The Movies
+Jacob Garneau
+
+The Truman Game - A game where you are the producer of the Truman Show. Make as much money as possible without the star of the show realizing that his entire life is fake!
+*/
+
+function preload() {}
+
+function setup() {}
+
+function draw() {}
