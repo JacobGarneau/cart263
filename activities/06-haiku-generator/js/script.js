4a0c1ecfb62805088a590ff69f04f65d79cbf70c
Jacob Garneau <jacobg17@hotmail.fr>
2021-03-11 12:38:34 -0500

    Activity 6: Added haiku generator
    Added code so that the webpage will generate a random haiku from pre-entered lines
    Added  CSS styling

--- activities/06-haiku-generator/js/script.js
+++ activities/06-haiku-generator/js/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /**
 Title of Project
 Author Name
@@ -8,25 +6,39 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-/**
-Description of preload
-*/
-function preload() {
+"use strict";
 
-}
+// Variables and objects
+let fiveSyllableLines = [
+  "O, to be a tree",
+  "The cat does not know",
+  "We are all forests",
+  "You have done your best",
+  "They are all gone now",
+];
+let sevenSyllableLines = [
+  "Say the things left unsaid",
+  "Never believe the wind's lies",
+  "The autumn stretches its legs",
+  "Nothing can satisfy you",
+  "They will not come back again",
+];
 
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
 
-/**
-Description of setup
-*/
-function setup() {
+let line1P = document.getElementById("line-1");
+let line2P = document.getElementById("line-2");
+let line3P = document.getElementById("line-3");
 
-}
+//  Program
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
 
-
-/**
-Description of draw()
-*/
-function draw() {
-
+//  Function definitions
+function random(array) {
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
