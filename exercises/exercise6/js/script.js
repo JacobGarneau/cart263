e6f53cf07f3801fc928719c672c8b23d62ac0869
Jacob Garneau <jacobg17@hotmail.fr>
2021-03-18 15:33:15 -0400

    E6: Started Exercise 6
    Copied files
    Changed HTML title
    Changed description
    Removed default assets

---
+++ exercises/exercise6/js/script.js
@@ -0,0 +1,34 @@
+/**
+Raving Redactionist ++
+Jacob Garneau
+
+Protect the secrets of the motherland!
+*/
+
+"use strict";
+
+// Code goes here
+const REVEAL_PROBABLILTY = 0.1;
+const REVELATION_INTERVAL = 500;
+
+$(`.top-secret`).on(`click`, redact);
+
+setInterval(revelation, REVELATION_INTERVAL);
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let rng = Math.random();
+
+  if (rng < REVEAL_PROBABLILTY) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
+
+function redact() {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
