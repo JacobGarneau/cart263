55b136a17db7b7d89d4620a0a020fa17196bb3d6
Jacob Garneau <jacobg17@hotmail.fr>
2021-03-18 15:30:25 -0400

    Activity 7: Created JavaScript

--- activities/07-raving-redactionist/js/script.js
+++ activities/07-raving-redactionist/js/script.js
@@ -9,3 +9,27 @@ author, and this description to match your project!
 "use strict";
 
 // Code goes here
+const REVEAL_PROBABLILTY = 0.1;
+const REVELATION_INTERVAL = 500;
+
+$(`.top-secret`).on(`click`, redact);
+
+setInterval(revelation, REVELATION_INTERVAL);
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let rng = Math.random();
+
+  if (rng < REVEAL_PROBABLILTY) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
+
+function redact() {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
