c9b3bf95fb6a22d693e7182a4aaad5228c3fee99
Jacob Garneau <jacobg17@hotmail.fr>
2021-03-25 14:39:07 -0400

    E7: Started Exercise 7
    Copied files
    Removed default assets
    Changed code description

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,40 @@
+/**
+Code Taker++
+Jacob Garneau
+
+Crack the code and save the world!
+*/
+
+"use strict";
+
+// Code goes here
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+$(`.secret`).on(`mouseover`, function (event) {
+  $(this).addClass(`found`, 500);
+});
+
+$(`.secret`).draggable({ helper: "clone" });
+
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    let character = ui.draggable.text();
+
+    $(this).append(character);
+
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
