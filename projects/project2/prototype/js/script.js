749f13525797d39a456bea2e50c64c7dcb646be0
Jacob Garneau <jacobg17@hotmail.fr>
2021-03-30 20:09:31 -0400

    P2: Added minimap
    Added a minimap in the corner of the screen that shows the player's position on the entire map

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -44,6 +44,7 @@ function draw() {
   drawPlayer();
   movePlayer();
   changeTile();
+  drawMiniMap();
 }
 
 function setupPlayer() {
@@ -90,6 +91,42 @@ function drawMap() {
       );
     }
   }
+}
+
+function drawMiniMap() {
+  // draw the minimap
+  for (let i = 0; i < MAP_WIDTH; i++) {
+    for (let j = 0; j < MAP_HEIGHT; j++) {
+      let currentTile = map[i][j];
+
+      if (currentTile.biome === `lake`) {
+        fill(0, 150, 150, 150);
+      } else if (currentTile.biome === `snow`) {
+        fill(255, 255, 255, 150);
+      } else if (currentTile.biome === `mountains`) {
+        fill(150, 150, 150, 150);
+      }
+
+      // draw the map cells
+      push();
+      rectMode(CENTER);
+      rect(
+        width - 320 + (i * width) / 40,
+        height - 210 + (j * height) / 40,
+        width / 40,
+        height / 40
+      );
+      pop();
+    }
+  }
+
+  // draw the player's icon
+  fill(255, 0, 0);
+  ellipse(
+    width - 320 + (player.mapX * width) / 40,
+    height - 210 + (player.mapY * height) / 40,
+    10
+  );
 }
 
 function drawPlayer() {
